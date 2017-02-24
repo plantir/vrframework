938f19b6b20c3dcb59f8bfd0c1c610cd8e6d54e8
armin <arminkheirkhahan@gmail.com>
2017-02-24 11:30:15 +0330

    mix lumen framework with angularClass

---
+++ FrontEndFramework/client/app/common/hero/hero.controller.js
@@ -0,0 +1,8 @@
+class HeroController {
+  constructor() {
+    this.name = 'hero';
+    console.log('ish');
+  }
+}
+
+export default HeroController;
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,19 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$app->get('/api/new-path',function() use($app){
+    return response()->json(['name' => 'api', 'state' => 'CA']);
 
+});
+$app->get('/api/user',function() use($app){
+    return response()->json(['name' => 'Abigail', 'state' => 'CA']);
+
+});
 $app->get('/', function () use ($app) {
     return view('index',['title'=>'armin']);
+    // return response()->json(['name' => 'Abigail', 'state' => 'CA']);
+});
+$app->get('/path', function () use ($app) {
+    return 'Hello';
+    // return response()->json(['name' => 'Abigail', 'state' => 'CA']);
 });
