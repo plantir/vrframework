db121d24af77276cee4ff61d35116897dd1969de
armin <arminkheirkhahan@gmail.com>
2017-03-03 00:05:03 +0330

    change to Model

--- app/Model/User.php
+++ app/Model/User.php
@@ -4,11 +4,10 @@ namespace App\Model;
 
 use Illuminate\Auth\Authenticatable;
 use Laravel\Lumen\Auth\Authorizable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends IModel implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable;
 
